c87b3f16ef47be1e3bdbd2795c77377bc7282884
Georgi Parlakov <gparlakov@gmail.com>
2020-10-03 13:06:27 +0300

    wip add pagination capabilities

--- apps/conduit/src/app/articles/page.articles.ts
+++ apps/conduit/src/app/articles/page.articles.ts
@@ -1,16 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { map, withLatestFrom } from 'rxjs/operators';
 import { articlesURL, tagsURL } from '../core/api-urls';
 import { Loadable } from '../core/loadable';
 import { ArticlesPreviewResponse, TagsResponse } from '../core/types';
 import { ArticlePreview } from './articles.types';
 import { stringIsNotEmpty } from '../core/string-is-empty';
+export interface ArticlesFilter {
+  tag?: 'string'; //=AngularJS
+
+  //Filter by author:
+  author?: 'string'; //=jake
+
+  //Favorited by user:
+  favorited?: 'string'; //=jake
+
+  //Limit number of articles (default is 20):
+  limit: number; //=20
+
+  //Offset/skip number of articles (default is 0):
+  offset?: number; //=0
+}
+
+export const reset = 'reset';
 
 @Injectable({ providedIn: 'root' })
 export class PageArticles {
-  public tags = new Loadable<string[]>();
-  public articles = new Loadable<ArticlePreview[]>();
+  tags = new Loadable<string[]>();
+  articles = new Loadable<ArticlePreview[]>();
+  filter: ArticlesFilter;
 
   constructor(private readonly http: HttpClient) {}
 
@@ -22,31 +40,72 @@ export class PageArticles {
         .pipe(map((r) => r.tags.filter(stringIsNotEmpty)))
     );
 
+    this.loadArticles(null, reset);
+  }
+
+  private loadArticles(filter?: ArticlesFilter, doReset?: typeof reset) {
+    this.filter = filter || { limit: 20 };
     this.articles.clearCache();
     this.articles.load(() =>
-      this.http.get<ArticlesPreviewResponse>(articlesURL).pipe(
-        map((a) =>
-          a.articles.map((ar) => ({
-            profileImg: ar?.author?.image || 'generic-image',
-            author: ar?.author?.username || 'unknown author',
-            publishedOn: ar?.createdAt,
-            likes: ar?.favoritesCount,
-            slug: ar.slug,
-            title: ar.title,
-            shortDescription:
-              typeof ar.description === 'string'
-                ? ar.description.slice(
-                    0,
-                    ar.description.length > 400 ? 400 : ar.description.length
-                  )
-                : '-',
-          }))
+      this.http
+        .get<ArticlesPreviewResponse>(articlesURL, this.doFiltring(filter))
+        .pipe(
+          map(this.mapToArticlePreview()),
+          withLatestFrom(this.articles.data$),
+          map(([next, current]) => (doReset === reset ? next : [...current, ...next]))
         )
-      )
     );
   }
 
-  onLeavePage() {
-    this.tags.clearCache();
+  private mapToArticlePreview(): (value: ArticlesPreviewResponse, index: number) => { profileImg: string; author: string; publishedOn: string; likes: number; slug: string; title: string; shortDescription: string; }[] {
+    return (a) => a.articles.map((ar) => ({
+      profileImg: ar?.author?.image || 'generic-image',
+      author: ar?.author?.username || 'unknown author',
+      publishedOn: ar?.createdAt,
+      likes: ar?.favoritesCount,
+      slug: ar.slug,
+      title: ar.title,
+      shortDescription: typeof ar.description === 'string'
+        ? ar.description.slice(
+          0,
+          ar.description.length > 400 ? 400 : ar.description.length
+        )
+        : '-',
+    }));
   }
+
+  private doFiltring(filter: ArticlesFilter) {
+    return {
+      params: this.filter != null
+        ? {
+          tag: filter.tag,
+          author: filter.author,
+          favorited: filter.favorited,
+          offset: filter.offset != null &&
+            typeof filter.offset.toString === 'function'
+            ? filter.offset.toString()
+            : '',
+          limit: filter.limit != null &&
+            typeof filter.limit.toString === 'function'
+            ? filter.limit.toString()
+            : '',
+        }
+        : {},
+    };
+  }
+
+  onLeavePage() {
+  }
+
+  onLoadMoreArticles() {
+    const filter =
+      this.filter && isValidNumber(this.filter.offset)
+        ? { ...this.filter, offset: this.filter.offset + 20 }
+        : { offset: 0, limit: 20 };
+    this.loadArticles(filter);
+  }
+}
+
+function isValidNumber(v: any): v is number {
+  return v != null && (!isNaN(v) || !isNaN(parseInt(v, 10)));
 }
